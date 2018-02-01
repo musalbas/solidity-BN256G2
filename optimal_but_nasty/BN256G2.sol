bb35363f3d77118864569eefe46dcb777d979b72
Mustafa Al-Bassam <mus@musalbas.com>
2018-02-01 11:47:06 +0000

    Update BN256G2.sol

--- optimal_but_nasty/BN256G2.sol
+++ optimal_but_nasty/BN256G2.sol
@@ -9,6 +9,7 @@ library BN256G2 {
     
     function _submod(uint256 a, uint256 b, uint256 n) constant returns (uint256) {
         // assumes n >= b (true in our use case)
+        assert(b < n);
         return addmod(a, n - b, n);
     }
     
@@ -26,6 +27,11 @@ library BN256G2 {
     
     function _FQ2Sub(uint256 xx, uint256 xy, uint256 yx, uint256 yy) constant returns(uint256 rx, uint256 ry) {
         (rx, ry) = (_submod(xx, yx, FIELD_MODULUS), _submod(xy, yy, FIELD_MODULUS));
+    }
+    
+    function _FQ2Div(uint256 xx, uint256 xy, uint256 yx, uint256 yy) constant returns(uint256 rx, uint256 ry) {
+        (yx, yy) = _FQ2Inv(yx, yy);
+        (rx, ry) = _FQ2Mul(xx, xy, yx, yy);
     }
     
     function _FQ2Inv(uint256 x, uint256 y) constant returns(uint256 rx, uint256 ry) {
@@ -51,7 +57,7 @@ library BN256G2 {
             if (pt1zx == 0 && pt1zy == 0) {
                 (pt3[PTXX], pt3[PTXY], pt3[PTYX], pt3[PTYY], pt3[PTZX], pt3[PTZY]) = (pt2xx, pt2xy, pt2yx, pt2yy, pt2zx, pt2zy);
                 return;
-            } else if (pt2yx == 0 && pt2zy == 0) {
+            } else if (pt2zx == 0 && pt2zy == 0) {
                 (pt3[PTXX], pt3[PTXY], pt3[PTYX], pt3[PTYY], pt3[PTZX], pt3[PTZY]) = (pt1xx, pt1xy, pt1yx, pt1yy, pt1zx, pt1zy);
                 return;
             }
@@ -61,12 +67,12 @@ library BN256G2 {
             (pt2xx, pt2xy) = _FQ2Mul(pt2xx, pt2xy, pt1zx, pt1zy); // V1 = x2 * z1
             (pt3[PTZX], pt3[PTZY]) = _FQ2Mul(pt1xx, pt1xy, pt2zx, pt2zy); // V2 = x1 * z2
             
-            if (pt2xx == pt2zx && pt2xy == pt2zy) {
+            if (pt2xx == pt3[PTZX] && pt2xy == pt3[PTZY]) {
                 if (pt2yx == pt3[PTYX] && pt2yy == pt3[PTYY]) {
                     (pt3[PTXX], pt3[PTXY], pt3[PTYX], pt3[PTYY], pt3[PTZX], pt3[PTZY]) = ECDouble(pt1xx, pt1xy, pt1yx, pt1yy, pt1zx, pt1zy);
                     return;
                 }
-                (pt3[PTXX], pt3[PTXY], pt3[PTYX], pt3[PTYY], pt3[PTZX], pt3[PTZY]) = (1, 0, 1, 0, 0, 0);
+                (pt3[PTXX], pt3[PTXY], pt3[PTYX], pt3[PTYY], pt3[PTZX], pt3[PTZY]) = (0, 0, 0, 0, 0, 0);
                 return;
             }
             
@@ -76,6 +82,7 @@ library BN256G2 {
             (pt1zx, pt1zy) = _FQ2Mul(pt1yx, pt1yy, pt1yx, pt1yy); // V_squared = V * V
             (pt2yx, pt2yy) = _FQ2Mul(pt1zx, pt1zy, pt3[PTZX], pt3[PTZY]); // V_squared_times_V2 = V_squared * V2
             (pt1zx, pt1zy) = _FQ2Mul(pt1zx, pt1zy, pt1yx, pt1yy); // V_cubed = V * V_squared
+            
             (pt3[PTZX], pt3[PTZY]) = _FQ2Mul(pt1zx, pt1zy, pt2zx, pt2zy); // newz = V_cubed * W
             (pt2xx, pt2xy) = _FQ2Mul(pt1xx, pt1xy, pt1xx, pt1xy); // U * U
             (pt2xx, pt2xy) = _FQ2Mul(pt2xx, pt2xy, pt2zx, pt2zy); // U * U * W
@@ -112,19 +119,20 @@ library BN256G2 {
         (pt2zx, pt2zy) = _FQ2Mulc(pt2zx, pt2zy, 8); // newz = 8 * S * S_squared
     }
     
-    /*function ECMul(uint256 d, uint256 pt1xx, uint256 pt1xy, uint256 pt1yx, uint256 pt1yy, uint256 pt1zx, uint256 pt1zy) constant returns(uint256[6] pt2) {
-        (pt2[PTXX], pt2[PTXY], pt2[PTYX], pt2[PTYY], pt2[PTZX], pt2[PTZY]) = (1, 0, 1, 0, 0, 0);
-        if (d == 0) {
-            return;
-        }
-        
-        uint256 n = 0;
-        while (n <= 255) {
-            (pt2[PTXX], pt2[PTXY], pt2[PTYX], pt2[PTYY], pt2[PTZX], pt2[PTZY]) = ECDouble(pt2[PTXX], pt2[PTXY], pt2[PTYX], pt2[PTYY], pt2[PTZX], pt2[PTZY]);
-            if (d & (1 >> (255 - n)) != 0) {
+    function ECMul(uint256 d, uint256 pt1xx, uint256 pt1xy, uint256 pt1yx, uint256 pt1yy, uint256 pt1zx, uint256 pt1zy) constant returns(uint256[6] pt2) {
+        bytes32 b = bytes32(d);
+        uint n;
+        while (d != 0) {
+            if (pt2[PTZX] != 0 || pt2[PTZY] != 0) {
+                (pt2[PTXX], pt2[PTXY], pt2[PTYX], pt2[PTYY], pt2[PTZX], pt2[PTZY]) = ECDouble(pt2[PTXX], pt2[PTXY], pt2[PTYX], pt2[PTYY], pt2[PTZX], pt2[PTZY]);
+            }
+            if ((b[n / 8] >> (7 - (n % 8))) & 1 != 0) {
                 pt2 = ECAdd(pt2[PTXX], pt2[PTXY], pt2[PTYX], pt2[PTYY], pt2[PTZX], pt2[PTZY], pt1xx, pt1xy, pt1yx, pt1yy, pt1zx, pt1zy);
+            }
+            if (pt2[PTZX] != 0 || pt2[PTZY] != 0) {
+                d = d / 2;
             }
             n += 1;
         }
-    }*/
+    }
 }
