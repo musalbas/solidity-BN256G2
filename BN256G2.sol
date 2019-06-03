42230d81dc401911c3e152d432c27f51a514098f
Mustafa Al-Bassam <mus@musalbas.com>
2019-06-03 15:05:29 +0100

    Add repo URL

--- BN256G2.sol
+++ BN256G2.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.24;
 /**
  * @title Elliptic curve operations on twist points for alt_bn128
  * @author Mustafa Al-Bassam (mus@musalbas.com)
+ * @dev Homepage: https://github.com/musalbas/solidity-BN256G2
  */
 
 library BN256G2 {
